dc115cebc6f0f9b1441f4ab2329547fab28671a6
fabiodemo <faberdemo@gmail.com>
2022-11-30 21:52:02 -0300

    Alterações no jogo

---
+++ truco/jogo.py
@@ -0,0 +1,43 @@
+from baralho import Baralho
+from jogador import Jogador
+import random
+
+class Jogo():
+
+    def __init__(self):
+        self.rodadas = []
+    
+    def iniciarJogo(self):
+        pass
+
+    def criarJogador(self, nome, baralho):
+        jogador = Jogador(nome)
+        jogador.criarMao(baralho)
+        return jogador
+
+    def verificarGanhador(self, carta1, carta2):
+        print(carta1.numero)
+        print(carta2.numero)
+    
+
+    def quemJogaPrimeiro(jogador1, jogador2, carta1, carta2, ganhador):
+        if carta1 == ganhador:
+            jogador1.primeiro = True
+            jogador2.primeiro = False
+        elif carta2 == ganhador:
+            jogador1.primeiro = False
+            jogador2.primeiro = True
+        elif ganhador == "Empate":
+            pass
+
+    def quemIniciaRodada(self, jogador1, jogador2):
+        if jogador1.pontos == 0 and jogador2.pontos == 0:
+            if jogador1.ultimo == True:
+                jogador2.ultimo = True
+                jogador1.ultimo = False
+                jogador1.primeiro = True
+                jogador2.primeiro = False
+            elif jogador2.ultimo == True:
+                jogador2.ultimo = False
+                jogador1.primeiro = False
+                jogador2.primeiro = True
