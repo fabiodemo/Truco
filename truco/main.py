988c23ffb69c505f39c17f193d8f60e9ebb4a900
fabiodemo <faberdemo@gmail.com>
2023-01-08 22:58:46 -0300

    Border box na msg

--- truco/main.py
+++ truco/main.py
@@ -20,6 +20,20 @@ def reiniciarJogo():
 def limpar():
     os.system("clear")
 
+def border_msg(msg, indent=1, width=None, title=None):
+    """Print message-box with optional title."""
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    print(box)
+
 if __name__ == '__main__':
     jogo = Jogo()
     baralho = Baralho()
@@ -144,10 +158,7 @@ if __name__ == '__main__':
                 print(f"\n{jogador2.nome} ganhou a rodada")
                 reiniciarJogo()
 
-        print("____________________________")
-        print(f"\n|{jogador1.nome} Pontos {jogador1.pontos}, Rodadas {jogador1.rodadas}|")
-        print(f"|{jogador2.nome} Pontos {jogador2.pontos}, Rodadas {jogador2.rodadas}|")
-        print("____________________________")
+        border_msg(f"Jogador 1: {jogador1.nome} Pontos {jogador1.pontos}, Rodadas {jogador1.rodadas}\nJogador 2: {jogador2.nome} Pontos {jogador2.pontos}, Rodadas {jogador2.rodadas}")
 
         jogo.quemIniciaRodada(jogador1, jogador2)
 
