2507a54ff231eef8012feb6b725795783152a7f7
fabiodemo <faberdemo@gmail.com>
2023-01-31 19:57:41 -0300

    Ajustes nos dados da partida

--- truco/dados.py
+++ truco/dados.py
@@ -4,27 +4,53 @@ import os
 class Dados():
 
     def __init__(self):
-        self.modeloRegistro = pd.read_csv('../modelo_registro.csv', index_col='idMao')
+        self.registro_modelo = pd.read_csv('../modelo_registro.csv', index_col='idMao')
         self.dados = pd.read_csv('../dbtrucoimitacao_maos.csv', index_col='idMao').fillna(0)
 
-    def inicializarRegistro(self):
+    def inicializar_jogada(self, potuacao_cartas, mao_rank):
         
-        self.modeloRegistro.jogadorMao = 1
-        self.modeloRegistro.cartaAltaRobo = self.pontuacaoCartas[self.maoRank.index("Alta")]
-        self.modeloRegistro.cartaMediaRobo = self.pontuacaoCartas[self.maoRank.index("Media")]
-        self.modeloRegistro.cartaBaixaRobo = self.pontuacaoCartas[self.maoRank.index("Baixa")]
-        self.modeloRegistro.ganhadorPrimeiraRodada = 2
-        self.modeloRegistro.ganhadorSegundaRodada = 2
-        self.modeloRegistro.ganhadorTerceiraRodada = 2
+        self.registro_modelo.jogadorMao = 1
+        self.registro_modelo.cartaAltaRobo = potuacao_cartas[mao_rank.index("Alta")]
+        self.registro_modelo.cartaMediaRobo = potuacao_cartas[mao_rank.index("Media")]
+        self.registro_modelo.cartaBaixaRobo = potuacao_cartas[mao_rank.index("Baixa")]
+        self.registro_modelo.ganhadorPrimeiraRodada = 2
+        self.registro_modelo.ganhadorSegundaRodada = 2
+        self.registro_modelo.ganhadorTerceiraRodada = 2
 
-    def exportar_jogadas_csv(self, df):
+        return self.registro_modelo
+
+    def segunda_jogada(self, primeira_carta_robo, primeira_carta_humano):
+        self.registro_modelo.primeiraCartaRobo = primeira_carta_robo.retornarNumero()
+        self.registro.naipePrimeiraCartaRobo = primeira_carta_robo.retornarNaipe()
+        self.registro_modelo.primeiraCartaHumano = primeira_carta_humano.retornarNumero()
+        self.registro_modelo.naipePrimeiraCartaHumano = primeira_carta_humano.retornarNaipe()
+    
+    def terceira_jogada(self, segunda_carta_robo, segunda_carta_humano):
+        self.registro_modelo.segundaCartaRobo = segunda_carta_robo.retornarNumero()
+        self.registro.naipeSegundaCartaRobo = segunda_carta_robo.retornarNaipe()
+        self.registro_modelo.SegundaCartaHumano = segunda_carta_humano.retornarNumero()
+        self.registro_modelo.naipeSegundaCartaHumano = segunda_carta_humano.retornarNaipe()
+
+    def finalizar_jogadas(self, terceira_carta_robo, terceira_carta_humano):
+        self.registro_modelo.terceiraCartaRobo = terceira_carta_robo.retornarNumero()
+        self.registro.naipeTerceiraCartaRobo = terceira_carta_robo.retornarNaipe()
+        self.registro_modelo.terceiraCartaHumano = terceira_carta_humano.retornarNumero()
+        self.registro_modelo.naipeTerceiraCartaHumano = terceira_carta_humano.retornarNaipe()
+
+    def finalizar_jogo(self, df):
         if not(os.path.isfile('jogadas.csv')):
             df.to_csv('jogadas.csv', header=df.columns)
         else:
             df.to_csv('jogadas.csv', mode='a', header=False)
 
-        # self.modeloRegistro.to_csv('jogadas.csv')
+        # self.registro_modelo.to_csv('jogadas.csv')
+
+    def subset_envido(self):
+        pass
+
+    def subset_truco(self):
+        pass
 
     def carregar_modelo_zerado(self):
-        registro = pd.read_csv('../modelo_registro.csv', index_col='idMao')
-        return registro
+        self.registro_modelo = pd.read_csv('../modelo_registro.csv', index_col='idMao')
+        return self.registro_modelo
