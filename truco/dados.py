7521566f064da98feb7ebcdf460c1c380e1c1e08
fabiodemo <faberdemo@gmail.com>
2023-01-31 07:53:16 -0300

    Salvar ou adicionar ao fim do arquivo de jogadas

--- truco/dados.py
+++ truco/dados.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import os
 
 class Dados():
 
     def __init__(self):
         self.modeloRegistro = pd.read_csv('../modelo_registro.csv', index_col='idMao')
+        self.dados = pd.read_csv('../dbtrucoimitacao_maos.csv', index_col='idMao').fillna(0)
 
     def inicializarRegistro(self):
+        
         self.modeloRegistro.jogadorMao = 1
         self.modeloRegistro.cartaAltaRobo = self.pontuacaoCartas[self.maoRank.index("Alta")]
         self.modeloRegistro.cartaMediaRobo = self.pontuacaoCartas[self.maoRank.index("Media")]
@@ -14,8 +17,13 @@ class Dados():
         self.modeloRegistro.ganhadorSegundaRodada = 2
         self.modeloRegistro.ganhadorTerceiraRodada = 2
 
-    def inserir_csv(self):
-        self.modeloRegistro.to_csv('jogadas.csv')
+    def exportar_jogadas_csv(self, df):
+        if not(os.path.isfile('jogadas.csv')):
+            df.to_csv('jogadas.csv', header=df.columns)
+        else:
+            df.to_csv('jogadas.csv', mode='a', header=False)
+
+        # self.modeloRegistro.to_csv('jogadas.csv')
 
     def carregar_modelo_zerado(self):
         registro = pd.read_csv('../modelo_registro.csv', index_col='idMao')
