c4372a010cf167dccc8e84b14e4cc3c0236a033b
fabiodemo <faberdemo@gmail.com>
2023-02-01 13:53:20 -0300

    Criação da classe flor

--- truco/flor.py
+++ truco/flor.py
@@ -2,6 +2,9 @@ class Flor():
 
     def __init__(self):
         self.valor_flor = 3
+        self.quem_pediu_flor = 0
+        self.quem_pediu_contraflor = 0
+        self.quem_pediu_contraflor_resto = 0
 
     def pedir_flor(self, jogador1, jogador2):
         if (jogador1.flor and jogador2.flor):
@@ -22,6 +25,33 @@ class Flor():
         if jogador1_pontos > jogador2_pontos:
             jogador1.pontos += self.valor_flor
         elif jogador2_pontos > jogador1_pontos:
-            jogador1.pontos += self.valor_flor
+            jogador2.pontos += self.valor_flor
         else:
             quem_pediu.pontos += self.valor_flor
+
+    def pedir_contraflor(self, quem_pediu, jogador1, jogador2):
+        self.valor_flor = 6
+        jogador1_pontos = jogador1.calculaEnvido()
+        jogador2_pontos = jogador2.calculaEnvido()
+        if jogador1_pontos > jogador2_pontos:
+            jogador1.pontos += self.valor_flor
+        elif jogador2_pontos > jogador1_pontos:
+            jogador2.pontos += self.valor_flor
+        else:
+            quem_pediu.pontos += self.valor_flor
+
+    def pedir_contraflor_resto(self, quem_pediu, jogador1, jogador2):
+        jogador1_pontos = jogador1.calculaEnvido()
+        jogador2_pontos = jogador2.calculaEnvido()
+        if jogador1_pontos > jogador2_pontos:
+            jogador1.pontos += 12 - jogador2.pontos
+        elif jogador2_pontos > jogador1_pontos:
+            jogador2.pontos += 12 - jogador1.pontos
+        else:
+            quem_pediu.pontos += self.valor_flor
+
+    def resetar_flor(self):
+        self.valor_flor = 3
+        self.quem_pediu_flor = 0
+        self.quem_pediu_contraflor = 0
+        self.quem_pediu_contraflor_resto = 0
